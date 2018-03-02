b548a8efa09e1101f7560f48dd74ce0ff953a793
Ziλa Sarikaya <sarikayaziya@gmail.com>
2018-03-02 12:09:52 +0100

    Refactor stats route

--- src/main/scala/org/reducio/Router.scala
+++ src/main/scala/org/reducio/Router.scala
@@ -2,7 +2,7 @@ package org.reducio
 
 import akka.http.scaladsl.server.{ Directives, Route }
 import de.heikoseeberger.akkahttpcirce.FailFastCirceSupport
-import org.reducio.Routes.{ GetShortenedRoute, ShortenRoute, StatsRoute }
+import org.reducio.routes.{ GetShortenedRoute, ShortenRoute, StatsRoute }
 import org.reducio.services.UrlShortenerService
 
 class Router(urlService: UrlShortenerService)
--- src/main/scala/org/reducio/routes/StatsRoute.scala
+++ src/main/scala/org/reducio/routes/StatsRoute.scala
@@ -1,6 +1,7 @@
 package org.reducio.routes
 
-import java.net.URL
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
 
 import akka.http.scaladsl.model.StatusCodes._
 import akka.http.scaladsl.server.{ Directives, Route }
@@ -9,25 +10,27 @@ import io.circe.syntax._
 import org.reducio.HttpConfig
 import org.reducio.models.Stats
 import org.reducio.services.UrlShortenerService
+import org.reducio.util.HttpUtils._
 
 case class StatsRoute(urlService: UrlShortenerService)
   extends Directives
   with FailFastCirceSupport
   with HttpConfig {
 
+  def validateAndGetStats(url: String): Future[Option[Stats]] = for {
+    uriOpt: Option[String] <- validateUri(url)
+    statsResult <- uriOpt match {
+      case Some(uri) => urlService.stats(uri.toString)
+      case None => Future(None)
+    }
+  } yield statsResult
+
   val routes: Route =
     pathPrefix("stats"./) {
       parameter('url) { url =>
-        try {
-          val uri = new URL(url)
-          onSuccess(urlService.stats(uri.toString)) {
-            case Some(stats: Stats) =>
-              complete(OK, stats.asJson)
-            case None => complete(NotFound)
-          }
-        } catch {
-          case _: Throwable =>
-            complete(BadRequest)
+        onSuccess(validateAndGetStats(url)) {
+          case Some(stats: Stats) => complete(OK, stats.asJson)
+          case None => complete(NotFound)
         }
       }
     }
